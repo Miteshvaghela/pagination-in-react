e3225c97d6ed39e89f902513cc9c5fdeb3bdf6fb
Miteshvaghela <vaghela_miteshk@yahoo.com>
2024-04-07 19:40:34 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,67 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import axios from 'axios';
 import PostItem from './components/PostItem';
+import Pagination from './components/Pagination';
 
 function App() {  
   const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [recordPerPage, setRecordPerPage] = useState(1);
+  const indexOfLastRecord = currentPage * recordPerPage;
+  const indexOfFirstRecord = indexOfLastRecord - recordPerPage;
+  const currentRecords = posts.slice(indexOfFirstRecord, 
+    indexOfLastRecord);
 
   useEffect(() => {
+    setLoading(true);
     fetchPosts();
   }, [])
 
   const fetchPosts = async () => {
-    const fetch = await axios.get('Data.json');
-    setPosts(fetch.data);
+    // await fetch('Data.json', {
+    //     method : 'GET',
+    //     headers : {
+    //       'Content-Type':'application/json'
+    //     }
+    //   })
+    //   .then(data => {
+    //     return data.json()
+    //   })
+    //   .then((response) => {
+    //     setPosts(response) 
+    //   })
+
+    await axios.get('Data.json')
+        .then(data => {
+          setPosts(data.data);
+        })
+        .catch(error => {
+          console.error(error);
+        })
   }
+
+
+  const nPages = Math.ceil(posts.length / recordPerPage)
 
   return (
     <div className="app container">
-      <h2>Pagination in React</h2>
+      <h1 style={{textAlign:'center', margin:'40px auto'}}>Pagination in React</h1>
       <div className="posts">
-        {posts.length ? (posts.map(post => <PostItem key={post.id} post={post} />)) : 'No Posts'}
+        {currentRecords.length ? (currentRecords.map(post => <PostItem key={post.id} post={post} />)) : 'No Posts'}
       </div>
+      <Pagination
+                nPages={nPages}
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+            />
     </div>
   )
 }
 
 export default App
+
+
+
+// https://github.com/Miteshvaghela/pagination-in-react.git
